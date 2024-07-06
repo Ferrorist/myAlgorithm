9a9f6e14417ebf165577b00a41521fe92e1a1ef8
Ferrorist <ferrorist@naver.com>
2024-07-06 19:39:11 +0900

    sovle: P59046.sql

---
+++ 2024/July/240706/P59046.sql
@@ -0,0 +1,6 @@
+/*
+ * https://school.programmers.co.kr/learn/courses/30/lessons/59046
+ * 소요 시간 : 1분 49초
+ */
+SELECT ANIMAL_ID, NAME, SEX_UPON_INTAKE FROM ANIMAL_INS
+WHERE NAME IN ("Lucy", "Ella", "Pickle", "Rogan", "Sabrina", "Mitty");
