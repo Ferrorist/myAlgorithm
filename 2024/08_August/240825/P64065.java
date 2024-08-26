0a6e7b2e38e424b387edfac1304f4842e8bfddb6
parkdamin <ferrorist@naver.com>
2024-08-26 18:18:31 +0900

    fix: P64065.java

--- 2024/08_August/240825/P64065.java
+++ 2024/08_August/240825/P64065.java
@@ -5,21 +5,18 @@
 import java.util.*;
 class Solution {
     public int[] solution(String s) {              
-        s = s.substring(1, s.length() - 1);
-        String[] input = s.split("},");
+        String[] input = s.substring(1, s.length() - 1).split("},");
         Map<Integer, Integer> map = new HashMap<>();
         
-        int max_length = 0;
         for(String str : input) {    
             String[] nums = str.replace("{", "").replace("}", "").split(",");    
             for(int i = 0; i < nums.length; i++)    {
                 int value = Integer.parseInt(nums[i]);
                 map.put(value, map.getOrDefault(value, 0) + 1);   
             } 
-            max_length = Math.max(max_length, nums.length);
         }
         
-        int[] answer = new int[max_length];
+        int[] answer = new int[map.size()];
         List<Integer> keySet = new ArrayList<>(map.keySet());
         
         keySet.sort((o1, o2) -> map.get(o2).compareTo(map.get(o1)));
@@ -27,8 +24,7 @@ class Solution {
         for(Integer i : keySet){
             answer[idx++] = i;
         }
-        
-        
+    
         return answer;
     }
 }
